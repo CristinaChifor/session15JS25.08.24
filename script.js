972b6a6536da6475d086e9134861a609c31b7286
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-08-25 20:00:36 +0300

    Add basic class and object examples

--- script.js
+++ script.js
@@ -23,3 +23,27 @@ obiect din clasa si are rol de initializare obiect. Scopul principal al construc
 configureze obiectul si sa atribuie valori initiale proprietatilor acestuia.
    */
 
+class Product {
+    constructor(productName, price, stockQuantity) {
+        this.productName = productName;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+}
+
+const product1 = new Product("Bike", 750, 1000);
+console.log(product1);
+
+
+const product2 = {
+    productName : "Bike",
+    price: 750,
+    stockQuantity: 1000
+}; 
+//exemplu de tip Object - key: value
+console.log(product2);
+
+const product3 = new Product("Laptop", 2500, 300);
+console.log(product3);
+
+
