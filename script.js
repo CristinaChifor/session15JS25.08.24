d5fb8b8d59184db38889a6441d73faae01727ae6
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-08-25 19:43:44 +0300

    Adds definitions

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("Hello!");
+
+// Object-Oriented Programming
+
+/** OOP, sau Programarea Orientata pe Obiect (in engleză, Object-Oriented Programming), este o paradigma de 
+ programare care utilizeaza conceptul de obiecte pentru 
+ a organiza si structura codul sursa. 
+ */
+
+ /** OOP - Clase
+  * Clasele sunt tipare pentru obiecte si sunt folosite 
+  *pentru a crea obiecte cu proprietati si comportament similar.
+  */
+
+  /** Sintaxa: class NumeClasa { constructor(){} alteMembre… }
+
+  class - defineste inceputul definitiei unei clase;
+NumeClasa - numele dat de noi clasei (trebuie sa fie un nume reprezentativ pentru clasa). 
+Acesta trebuie sa respecte notatia PascalCase;
+
+constructor - este o metoda (functie ce apartine clasei) speciala ce se apelează in momentul crearii unui nou 
+obiect din clasa si are rol de initializare obiect. Scopul principal al constructorului este sa 
+configureze obiectul si sa atribuie valori initiale proprietatilor acestuia.
+   */
+
